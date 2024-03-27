96c75020dbd67ffe5ee7327cff395afe4de4a932
David <dawid.szcz@gmail.com>
2024-03-27 12:44:26 +0200

    part3 lists done

--- part03-Part03_16.Sum/src/main/java/Sum.java
+++ part03-Part03_16.Sum/src/main/java/Sum.java
@@ -5,6 +5,28 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
     }
 
+
+    public static int sum(ArrayList<Integer> numbers){
+        int sum1 = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            int num = numbers.get(i);
+
+            sum1 = sum1 + num;
+        }
+        
+        return sum1;
+
+    }
 }
