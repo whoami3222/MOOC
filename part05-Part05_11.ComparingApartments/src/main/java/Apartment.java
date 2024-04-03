12cc9722a0c790da7658a28fff8c6a0ec41010d9
David <dawid.szcz@gmail.com>
2024-04-03 15:31:01 +0300

    Part5 utpo 13

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,26 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared){
+        if(this.squares > compared.squares){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int priceDifference(Apartment compared){
+        
+        return Math.abs((this.squares * this.princePerSquare) - (compared.squares * compared.princePerSquare));
+           
+    }
+
+    public boolean moreExpensiveThan(Apartment compared){
+        if((this.squares * this.princePerSquare)>(compared.squares * compared.princePerSquare)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -16,4 +16,28 @@ public class Person {
     }
 
     // implement an equals method here for checking the equality of objects
+
+    public boolean equals (Object compared){
+        if(this == compared){
+            return true;
+        }
+
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        // let's cast the object to SimpleDate
+        Person otherPerson = (Person) compared;
+
+        // if the object's values are the same, the objects are the same
+        if (this.name == otherPerson.name
+                && this.birthday.equals(otherPerson.birthday)
+                && this.height == otherPerson.height
+                && this.weight == otherPerson.weight) {
+            return true;
+        }
+
+        // other wise, the objects are not the same
+        return false;
+    }
 }
