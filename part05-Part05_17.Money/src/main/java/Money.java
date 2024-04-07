33b7651d187fc9f5b363f30c5818809f63d6fddd
David <dawid.szcz@gmail.com>
2024-04-07 23:23:35 +0300

    Part5 done

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -32,4 +32,56 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros + addition.euros,this.cents + addition.cents); // create a new Money object that has the correct worth
+        
+        // return the new Money object
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared){
+        if(this.euros < compared.euros){
+            
+            return true;
+        }else if(this.euros == compared.euros){
+            if(this.cents < compared.cents){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+
+    public Money minus(Money decreaser){
+         // create a new Money object that has the correct worth
+        Money newMoneys = new Money(this.euros - decreaser.euros,this.cents - decreaser.cents);
+        int cents = this.cents;
+        int euros = this.euros;
+        if(this.lessThan(decreaser)){
+            newMoneys = new Money(0,0);
+            return newMoneys;
+        }
+        if(this.cents < decreaser.cents){
+            cents = cents + 100;
+            cents = cents - decreaser.cents;
+
+            euros = euros - 1;
+            euros = euros - decreaser.euros;
+        }else{
+            cents = cents - decreaser.cents;
+            euros = euros - decreaser.euros;
+        }
+
+        newMoneys = new Money(euros, cents);
+        return newMoneys;
+
+        
+        }
+        
+        // return the new Money object
+        
 }
+
+
