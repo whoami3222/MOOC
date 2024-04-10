bba0716c03ea03b0104490168f36bd9fe04b8761
David <dawid.szcz@gmail.com>
2024-04-10 14:48:08 +0300

    part6_05

--- part06-Part06_05.SantasWorkshop/src/main/java/Main.java
+++ part06-Part06_05.SantasWorkshop/src/main/java/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
 
         // use this main method to try out your classes!
+        Gift book = new Gift("Harry Potter and the Philosopher's Stone", 2);
 
+        Package gifts = new Package();
+        gifts.addGift(book);
+        System.out.println(gifts.totalWeight());
     }
 }
