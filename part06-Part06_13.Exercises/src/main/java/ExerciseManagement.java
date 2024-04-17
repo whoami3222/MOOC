e0e6ef3b87b4a36e1f9dfe65fdb49fe33f16665f
David <dawid.szcz@gmail.com>
2024-04-17 20:30:25 +0300

    Part6 Finished

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,40 @@
 
-public class ExerciseManagement {
+import java.util.ArrayList;
 
+public class ExerciseManagement {
+    private ArrayList<Exercise> exercises;
+
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+    }
+
+    public ArrayList<String> exerciseList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (Exercise exercise : exercises) {
+            list.add(exercise.getName());
+        }
+        return list;
+    }
+
+    public void add(String exerciseName) {
+        this.exercises.add(new Exercise(exerciseName));
+    }
+
+    public void markAsCompleted(String exerciseName) {
+        for (Exercise ex : exercises) {
+            if (ex.getName().equals(exerciseName)) {
+                ex.setCompleted(true);
+                break; // Exit loop once exercise is found
+            }
+        }
+    }
+
+    public boolean isCompleted(String exerciseName) {
+        for (Exercise ex : exercises) {
+            if (ex.getName().equals(exerciseName)) {
+                return ex.isCompleted();
+            }
+        }
+        return false;
+    }
 }
